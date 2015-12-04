3fc92aec85f799668a93096fe3cce5c55331e20e
GullyFoyle2014 <alex.m2004@gmail.com>
2015-12-03 23:44:55 -0500

    adding sass compiler, adding .stop()
    Adding .stop() to slide toggles, preventing bubbling…  added contrib
    sass via grunt. Need to set up actual task!

--- js/solcom.js
+++ js/solcom.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     // More Details toggle for resume items
     $('.more-details h5 span').click(function(){
         var link = $(this);
-        $(this).parent().parent().children('ul').slideToggle('slow', function() {
+        $(this).parent().parent().children('ul').stop().slideToggle('slow', function() {
             if ($(this).is(':visible')) {
                  link.text('Show Less');
                  $(this).parent().parent().removeClass('hidden');
