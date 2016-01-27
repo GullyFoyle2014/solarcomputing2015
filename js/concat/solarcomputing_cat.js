19eb0fbe2046b864dad52104835327cd5c8a9397
GullyFoyle2014 <alex.m2004@gmail.com>
2016-01-27 14:19:21 -0500

    adding a greensock to scripts, a few small animations to resume, new resume pdf and initial json resume data file, not complete

--- js/concat/solarcomputing_cat.js
+++ js/concat/solarcomputing_cat.js
@@ -1,3 +1,16 @@
+//Animation Scripts
+
+window.onload = function(){
+    var logo = document.getElementById("logo");
+    var solContent = document.getElementById("sol-content");
+    var mainHd = document.getElementById("mainHd");
+    var awardsHd = document.getElementById("awardsHd");
+    var educationHd = document.getElementById("educationHd");
+    var experienceHd = document.getElementById("experienceHd");
+    
+    TweenMax.to(logo, 1, {right:"32px",opacity:1,scale:1.1,autoAlpha:1, ease: Power4.easeInOut});
+	TweenMax.fromTo([solContent,mainHd,awardsHd,educationHd,experienceHd], 2, {borderColor:"#dbf049", ease: Power4.easeInOut},{borderColor:"#9ab729", ease: Power4.easeInOut,repeat: 2});
+};
 $( document ).ready(function() {
 
     //No links prevent default
