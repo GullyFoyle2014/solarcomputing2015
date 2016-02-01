ef0ddf5dcb3583d2b0b076b7ad533d137fd7cc6c
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-01 18:35:23 -0500

    small fixes

--- js/anims.js
+++ js/anims.js
@@ -9,11 +9,16 @@ $( document ).ready(function() {
 	    var educationHd = document.getElementById("educationHd");
 	    var experienceHd = document.getElementById("experienceHd");
 	    var toTop = document.getElementById("toTop");
+	    var portFrame = $(".portImg");
 	    
 	    TweenMax.fromTo(logo, 0.75, {right:"-32px",opacity:1,scale:0.5,autoAlpha:1, ease: Power4.easeInOut}, {right:"16px",opacity:1,scale:1.0,autoAlpha:1, ease: Circ.easeInOut});
-		TweenMax.fromTo([solContent,mainHd,awardsHd,educationHd,experienceHd], 2, {borderColor:"#dbf049", ease: Power4.easeInOut},{borderColor:"#9ab729", ease: Power4.easeInOut,repeat: 1});
+		TweenMax.fromTo([solContent,mainHd,awardsHd,educationHd,experienceHd], 2, {borderColor:"#9ab729", ease: Power4.easeInOut},{borderColor:"#dbf049", ease: Power4.easeInOut,repeat:-1, yoyo:true});
 		TweenMax.fromTo([nav,solContent], 0.5, {backgroundPosition:"left", ease: Power0.easeInOut},{backgroundPosition:"-300px", ease: Power0.easeInOut},"-=5.0");
 		TweenMax.fromTo(mainHd, 0.5, {color:"#fff",marginLeft:-300,opacity:0,autoAlpha:1,ease: Power0.easeInOut},{color:"#666",marginLeft:-1, autoAlpha:1, opacity:1,ease: Power0.easeInOut},"-=0.1");
+		if( $('.portfolio')){
+			TweenMax.fromTo(portFrame, 5.0, {css:{borderColor:"#c7df63"}}, {css:{borderColor:"#8fae15"}, repeat:-1,yoyo:true} );
+		}
+
 
 		//TweenMax.to([solContent], 15, {backgroundPosition:"-300px", ease: Power4.easeInOut},{backgroundPosition:"left", ease: Power4.easeInOut});
 
