fbad2cc030e56c4af7191623f924abf357082ab8
GullyFoyle2014 <alex.m2004@gmail.com>
2016-02-08 23:35:42 -0500

    adding initial select val switch, hiding section content based on select option.  Need to remove H5s as well as section. need to animate the display if possible

--- js/solcom.js
+++ js/solcom.js
@@ -11,6 +11,8 @@ $( document ).ready(function() {
       $('.navcluster').removeClass('closed').addClass('open');
     }
 
+    //Sort portfolio items
+
 
     $('#menuTab').on('click', function(){
       $(this).parent().toggleClass('open');       
@@ -57,6 +59,80 @@ $( document ).ready(function() {
         $('#body').animate({
             scrollTop: 0
         }, 1500);
+    });
+
+    //portfolio select sort
+    $('#typeSelect').on('change', function() {
+        
+        var portSection = this.value;
+        var hearst = $('.portItem.hearst');
+        var penton = $('.portItem.penton');
+        var freelance = $('.portItem.freelance');
+        var spalab = $('.portItem.spalab');
+        var oven = $('.portItem.oven');
+        var agency = $('.portItem.agency');
+        var publishers = $('.portItem.publisher');
+        var allClients = [hearst,penton,freelance,spalab,oven,agency,publishers];
+
+
+          switch( portSection ) {
+
+              case 'all':
+                  //show all items
+                  console.log("case is all - show all items");
+                  break;
+              case 'hearst':
+                  //show hearst items
+                  console.log("case is hearst - show hearst items");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.hearst').removeClass('overflow');
+                  break;
+              case 'penton':
+                  //show penton items
+                  console.log("case is penton - show penton items");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.penton').removeClass('overflow');
+                  break;
+              case 'freelance':
+                  //show freelance items
+                  console.log("case is freelance - show freelance items");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.freelance').removeClass('overflow');
+                  break;
+              case 'spalab':
+                  //show spalab
+                  console.log("case is spalab - show spalab items");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.spalab').removeClass('overflow');
+                  break;
+              case 'oven':
+                  //show oven
+                  console.log("case is oven - show oven items");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.oven').removeClass('overflow');
+                  break;
+              case 'agency':
+                  //show agency work
+                  console.log("case is agency work - show agency items (oven, spalab, freelance)");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.agency').removeClass('overflow');
+                  break;
+              case 'publishers':
+                  //show publisher work
+                  console.log("case is publisher - show publisher work (hearst, penton, freelance, oven");
+                  $('.portItem').addClass('overflow');
+                  $('.portItem.publisher').removeClass('overflow');
+                  break;
+
+              default:
+                  //do default
+                  //show all as default
+                  console.log("case is All/default - show default/all items");
+          }
+
+
+        // console.log( this.value ); 
+        // or $(this).val()
     });
 
 });//end doc.ready
